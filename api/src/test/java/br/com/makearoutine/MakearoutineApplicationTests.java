679842306d20e9b021b93000ecfcc2754299bf5a
Felipe Bueno <felipebueno181@gmail.com>
2021-01-07 11:41:26 -0300

    iniciando api do projeto

---
+++ api/src/test/java/br/com/makearoutine/MakearoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.makearoutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakearoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
